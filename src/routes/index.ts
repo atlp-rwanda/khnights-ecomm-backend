436b61438c87dde8ad4354571de523df548da252
GSinseswa721 <glorianiyonkurusinseswa@gmail.com>
2024-04-24 06:24:16 -0700

    linking and formatting

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,10 @@
-import { Request, Response, Router } from "express";
-import { responseSuccess } from "../utils/response.utils";
-const router = Router();
-router.get("/status", (req: Request, res: Response) => {
-    return responseSuccess(res, 202, "This is a testing route that returns: 201");
+import { Request, Response, Router } from 'express'
+import { responseSuccess } from '../utils/response.utils'
+const router = Router()
+router.get('/status', (req: Request, res: Response) => {
+  return responseSuccess(res, 202, 'This is a testing route that returns: 201')
 })
 // All routes should be imported here and get export after specifying first route
 // example router.use("/stock". stockRoutes) =>:: First import stockRoutes and use it here, This shows how the route export will be handled
 
-export default router;
+export default router
