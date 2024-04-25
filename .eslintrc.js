6af2158aeb3dde708b3b52f02bc32815e577aaf0
GSinseswa721 <glorianiyonkurusinseswa@gmail.com>
2024-04-25 02:28:07 -0700

    fixing eslint and tsconfiguration

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+// // .eslintrc.js
+// module.exports = {
+// };
+module.exports = {
+    env: {
+      node: true, // Specify 'node' environment
+      es6: true,  // Specify ECMAScript version 6
+    },
+    // Other ESLint configurations...
+  };
