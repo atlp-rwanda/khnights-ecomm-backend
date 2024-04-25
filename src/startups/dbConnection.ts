74cd143d7270d98cef4b6a9fd6e29681f1ed43e2
Jean Paul Elisa NIYOKWIZERWA <mugishajeanbienvenu@gmail.com>
2024-04-25 13:49:33 +0200

    update

--- src/startups/dbConnection.ts
+++ src/startups/dbConnection.ts
@@ -1,42 +1,14 @@
-
-// Set up databae connection
-
-import { createConnection } from "typeorm";
+import { OrmConfig } from "../configs/db_config";
 
 const dbConnection = async () => {
-
-    const PORT = process.env.PORT;
-    const DB_HOST = process.env.DB_HOST;
-    const DB_USER = process.env.DB_USER;
-    const DB_PASS = process.env.DB_PASS;
-    const DB_NAME = process.env.DB_NAME;
-
-    if (!PORT) {
-        let port = 8080;
-    }
-
-    if (!DB_HOST) {
-        let dbHost = "localhost";
-    }
-    if (!DB_USER) {
-        let dbUser = "test";
-    }
-
-    if (!DB_PASS) {
-        let dbPass = "test";
-    }
-
-    if (!DB_NAME) {
-        let dbName = "test";
-    }
-
-    try {
-        await createConnection();
-        console.log("[db]: Database connected successfully");
-    } catch (error) {
-        console.log("[db]: Database connection failed");
-        console.log(error);
-    }
-}
+  await OrmConfig.initialize()
+    .then(() => {
+      console.log("[db]: Database connected successfully");
+    })
+    .catch((error) => {
+      console.log("[db]: Database connection failed");
+      console.log(error);
+    });
+};
 
 export { dbConnection };
