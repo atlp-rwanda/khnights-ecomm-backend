87e211280da5e7f04caee1e241b1b8ab21a074a8
Ndevu12 <niyokwizerwajeanpaulelisa@gmail.com>
2024-04-29 15:23:24 +0200

    updates: upadating dbConnection file for better practice

--- src/startups/dbConnection.ts
+++ src/startups/dbConnection.ts
@@ -1,14 +1,13 @@
-import { OrmConfig } from "../configs/db_config";
+import { OrmConfig } from '../configs/db_config';
 
 const dbConnection = async () => {
-  await OrmConfig.initialize()
-    .then(() => {
-      console.log("[db]: Database connected successfully");
-    })
-    .catch((error) => {
-      console.log("[db]: Database connection failed");
-      console.log(error);
-    });
+  try {
+    await OrmConfig.initialize();
+    console.log('[db]: Database connected successfully');
+  } catch (error) {
+    console.log('[db]: Database connection failed');
+    console.log(error);
+  }
 };
 
 export { dbConnection };
