02a90795df946ca242252b9dd10b1ce0b03fac3a
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2024-05-13 09:53:15 +0200

    feat- implement -Wish list

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: '*' }));
 app.use(router);
+
 addDocumentation(app);
 app.all('*', (req: Request, res: Response, next) => {
   const error = new CustomError(`Can't find ${req.originalUrl} on the server!`, 404);
