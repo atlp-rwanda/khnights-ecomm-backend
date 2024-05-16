40ed809b2746a6f119547355a77f46ade5b8dd13
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2024-05-16 11:06:12 +0200

    Ft-implment-wish list-functionalites

---
+++ src/controllers/wishListController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import{
+  addProductService,
+  getProductsService,
+  removeProductService,
+  clearAllProductService
+} from '../services'
+
+export const wishlistAddProduct = async (req: Request, res: Response) => {
+    await addProductService(req, res);
+  };
+
+  export const wishlistRemoveProduct = async (req: Request, res:Response) => {
+    await removeProductService(req, res);
+  }
+
+  export const wishlistGetProducts = async (req: Request, res:Response) => {
+    await getProductsService(req, res);
+  }
+
+  export const wishlistClearAllProducts = async (req: Request, res:Response) => {
+    await clearAllProductService(req, res);
+  }
