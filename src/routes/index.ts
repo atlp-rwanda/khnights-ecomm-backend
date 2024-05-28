5228fb00928f7d81993a4e32ac0d8102122ed8c2
GSinseswa721 <glorianiyonkurusinseswa@gmail.com>
2024-05-28 19:53:12 +0300

    test coverage

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
-import { Request, Response, Router } from 'express';
-import { responseSuccess } from '../utils/response.utils';
+import { Request, RequestHandler, Response, Router } from 'express';
+import { responseServerError, responseSuccess } from '../utils/response.utils';
 import userRoutes from './UserRoutes';
 import productRoutes from './ProductRoutes';
 import wishListRoutes from './wishListRoute';
@@ -7,6 +7,7 @@ import couponRoute from './couponRoutes';
 import cartRoutes from './CartRoutes';
 import feedbackRoute from './feedbackRoutes';
 import notificationRoute from './NoficationRoutes'
+import { authMiddleware } from '../middlewares/verifyToken';
 
 const router = Router();
 
@@ -21,5 +22,21 @@ router.use('/cart', cartRoutes);
 router.use('/coupons', couponRoute);
 router.use('/feedback', feedbackRoute);
 router.use('/notification', notificationRoute);
+
+// ROUTES FOR TESTING PURPOSE
+router.get('/test', (req: Request, res: Response) => {
+  res.status(200).json({ message: 'Route works!' });
+});
+router.post('/test/posting', (req: Request, res: Response) =>{
+  return responseSuccess(res, 200, req.body);
+});
+
+router.get('/test/secure', authMiddleware as RequestHandler, (req: Request, res: Response) =>{
+  responseSuccess(res, 200, 'This is a secured route.');
+});
+
+router.get('/test/error', (req: Request, res: Response) => {
+  responseServerError(res, 'This is server error route.');
+});
 
 export default router;
